a3e011304e430b2541f78f47483e7196a333a2c1
Zul <zm82@hw.ac.uk>
2015-02-11 16:33:53 +0400

    Minor fixes in Main added destinations + journeys text files

--- TaxiService/src/applicationLogic/Main.java
+++ TaxiService/src/applicationLogic/Main.java
@@ -12,28 +12,9 @@ import dataClasses.Journey;
 import dataClasses.Taxi;
 
 public class Main {
-	private static Set<Taxi> taxis;
-	private static List<Destination> destinations;
-	private static List<Journey> journeys2014;
-	private static List<Journey> journeys2015;
-
 	public static void main(String[] args) {
-		taxis = new TreeSet<Taxi>();
-		destinations = new ArrayList<Destination>();
-		journeys2015 = new ArrayList<Journey>();
-		journeys2014 = new ArrayList<Journey>();
-		
-		DestinationFileOps destinationFile = new DestinationFileOps(Helpers.DESTINATIONS_FILE_NAME);
-		destinations = destinationFile.getDestinations();
-		
-		TaxiFileOps taxiFile = new TaxiFileOps(Helpers.TAXIS_FILE_NAME);
-		taxis = taxiFile.getSortedTaxisAsc();
-		
 		JourneyFileOps journeyFile2014 = new JourneyFileOps(Helpers.DESTINATIONS_2014_FILE_NAME);
-		journeys2014 = journeyFile2014.getJourneys();
-		
 		JourneyFileOps journeyFile2015 = new JourneyFileOps(Helpers.JOURNEYS_2015_FILE_NAME);
-		journeys2015 = journeyFile2015.getJourneys();
 		
 		
 		TaxiFileOps.writeDriversAndDestinationsToFile(journeyFile2015.getDriversAndVisitedPlaces(), "DriversAndDestinations");
